e1f843667c5b103109d2be204c4462faf46e692d
Jerin George Mathew <jerin.edassery95@gmail.com>
2019-06-07 21:20:16 +0200

    set svg width and height based on browser window size

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,3 @@
-// BIG TODO: REFACTOR THIS CODE
-
 // svg items ids
 const CHRISTMAS_ITEMS = ["pine", 
 "top-left-circle", 
@@ -10,12 +8,15 @@ const CHRISTMAS_ITEMS = ["pine",
 "bottom-right-circle"
 ];
 
+let pageWidth = window.innerWidth
+let pageHeight = window.innerHeight
+
+// svg id to dataset field
 let idToKey = {};
 
 // onclick event listener function
 function triggerSort(xScale){
 	let selectedCircle = d3.select(this)
-	console.log(this)
 	let circleClass = selectedCircle.attr("class")
 	let sortKey = idToKey[circleClass]
 	sortBars(sortKey, xScale);
@@ -23,10 +24,11 @@ function triggerSort(xScale){
 
 function sortBars(key, xScale) {
 	let svg = d3.select("svg");
+
 	let sortedTrees = svg.selectAll("g.christmas-tree")
-	.sort(function(a, b) {
-		return d3.ascending(a[key], b[key]);
-	});
+						 .sort(function(a, b) {
+							 return d3.ascending(a[key], b[key]);
+						 });
 
 	CHRISTMAS_ITEMS.forEach(function(svgID){
 		sortedTrees.select("use." + svgID)
@@ -41,53 +43,58 @@ function sortBars(key, xScale) {
 	})
 };
 
-d3.json("data/small-data.json")
+function drawPlot(dataset){
+
+	let xScale = d3.scaleBand()
+					.domain(d3.range(dataset.length))
+					.rangeRound([0, pageWidth]) // edit this line
+					.paddingInner(0.1);
+		
+	//Select SVG element
+	let svg = d3.select("body")
+				.append("svg")
+				.attr("width", pageWidth)
+                .attr("height", pageHeight);
+	
+	//Create christmas trees placeholders
+	let christmasTrees = svg.selectAll("g.christmas-tree")
+							.data(dataset)
+							.enter()
+							.append("g")
+							.classed("christmas-tree", true)
+							.attr("pointer-events", "none");
+	
+	// put a pine and six christmas balls for each placeholder 
+	CHRISTMAS_ITEMS.forEach(function(svgID){
+		let item = christmasTrees.append("use")
+								 .attr("href", "images/christmas-tree.svg#" + svgID)
+								 .classed(svgID, true)
+								 .attr("x", function(d, i) {
+									 return xScale(i);
+								 })
+								 .attr("width", xScale.bandwidth())
+								 .attr("pointer-events", "auto");
+								
+								 if(svgID !== "pine"){
+									 item.on("click", function(){
+										 triggerSort.call(this, xScale)
+									 })
+										 .append("title") .text(function(d) {
+											 let key = idToKey[svgID];
+											 return "This value is " +  d[key]
+										 });
+		}
+
+	})
+}
+
+d3.json("data/data.json")
 .then(
 	function(data){
-		dataKeys = Object.keys(data[0])
+		dataKeys = Object.keys(data[0]);
 		console.table(data);
+		// populate idToKey object
 		CHRISTMAS_ITEMS.slice(1).forEach((key, idx) => idToKey[key] = dataKeys[idx]);
 		drawPlot(data);
 	}
 	);
-	
-	function drawPlot(dataset){
-		
-		let xScale = d3.scaleBand()
-		.domain(d3.range(dataset.length))
-		.rangeRound([0, 1250]) // edit this line
-		.paddingInner(0.1);
-		
-		//Select SVG element
-		let svg = d3.select("svg");
-		
-		//Create christmas trees placeholders
-		let christmasTrees = svg.selectAll("g.christmas-tree")
-		.data(dataset)
-		.enter()
-		.append("g")
-		.classed("christmas-tree", true)
-		.attr("pointer-events", "none");
-		
-		// put a pine and six christmas balls for each placeholder 
-		CHRISTMAS_ITEMS.forEach(function(svgID){
-			let item = christmasTrees.append("use")
-			.attr("href", "images/christmas-tree.svg#" + svgID)
-			.classed(svgID, true)
-			.attr("x", function(d, i) {
-				return xScale(i);
-			})
-			.attr("width", xScale.bandwidth())
-			.attr("pointer-events", "auto");
-			
-			if(svgID !== "pine"){
-				item.on("click", function(){
-					triggerSort.call(this, xScale)
-				})
-				.append("title") .text(function(d) {
-					let key = idToKey[svgID];
-					return "This value is " +  d[key]});
-				}
-				
-			})
-		}
